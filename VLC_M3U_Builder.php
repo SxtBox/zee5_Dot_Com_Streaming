4efa031f86cf8716a856785fdff80712764df2a5
Albdroid <sxt@usa.com>
2020-08-19 18:26:11 +0200

    Hosted Vesion

--- VLC_M3U_Builder.php
+++ VLC_M3U_Builder.php
@@ -1,25 +1,25 @@
 <?php
 
 /*
- +-------------------------------------------------------------+
+ ┌─────────────────────────────────────────────────────────────┐
  |  For More Modules Or Updates Stay Connected to Kodi dot AL  |
- +-------------------------------------------------------------+
- +-------------------------------------------------------------+
- ¦ Product   ¦ zee5.com Stream Extractor By HLS Url            ¦
- ¦ Version   ¦ v1.4-DEV                                        ¦
- ¦ Provider  ¦ https://www.zee5.com                            ¦
- ¦ Support   ¦ M3U8/VLC/KODI/SMART TV/XTream Codes/Web Players ¦
- ¦ Licence   ¦ MIT                                             ¦
- ¦ Author    ¦ Olsion Bakiaj                                   ¦
- ¦ Email     ¦ TRC4@USA.COM                                    ¦
- ¦ Author    ¦ Endrit Pano                                     ¦
- ¦ Email     ¦ INFO@ALBDROID.AL                                ¦
- ¦ Website   ¦ https://kodi.al                                 ¦
- ¦ Facebook  ¦ /albdroid.official/                             ¦
- ¦ Github    ¦ github.com/SxtBox/                              ¦
- ¦ Created   ¦ 18 August 2020                                  ¦
- ¦ Modified  ¦ 0000:00:00:ss                                   ¦
- +-------------------------------------------------------------+
+ └─────────────────────────────────────────────────────────────┘
+ ┌───────────┬─────────────────────────────────────────────────┐
+ │ Product   │ zee5.com Stream Extractor By HLS Url            │
+ │ Version   │ v1.4-DEV                                        │
+ │ Provider  │ https://www.zee5.com                            │
+ │ Support   │ M3U8/VLC/KODI/SMART TV/XTream Codes/Web Players │
+ │ Licence   │ MIT                                             │
+ │ Author    │ Olsion Bakiaj                                   │
+ │ Email     │ TRC4@USA.COM                                    │
+ │ Author    │ Endrit Pano                                     │
+ │ Email     │ INFO@ALBDROID.AL                                │
+ │ Website   │ https://kodi.al                                 │
+ │ Facebook  │ /albdroid.official/                             │
+ │ Github    │ github.com/SxtBox/                              │
+ │ Created   │ 18 August 2020                                  │
+ │ Modified  │ 0000:00:00:ss                                   │
+ └─────────────────────────────────────────────────────────────┘
 */
 
 // HOSTED VERSION https://paidcodes.albdroid.al/ZEE5_Dot_Com_Apis/
