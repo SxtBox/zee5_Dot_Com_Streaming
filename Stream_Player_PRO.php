1e50bbadfbec28a7d7922df8a59283dfbf4c9b28
Albdroid <sxt@usa.com>
2020-08-23 13:40:01 +0200

    Hosted Version

---
+++ Stream_Player_PRO.php
@@ -0,0 +1,41 @@
+
+<?php
+
+/*
+ ┌─────────────────────────────────────────────────────────────┐
+ |  For More Modules Or Updates Stay Connected to Kodi dot AL  |
+ └─────────────────────────────────────────────────────────────┘
+ ┌───────────┬─────────────────────────────────────────────────┐
+ │ Product   │ zee5.com Stream Extractor By HLS Url            │
+ │ Version   │ v1.6-DEV                                        │
+ │ Provider  │ https://www.zee5.com                            │
+ │ Support   │ M3U8/VLC/KODI/SMART TV/XTream Codes/Web Players │
+ │ Licence   │ MIT                                             │
+ │ Author    │ Olsion Bakiaj                                   │
+ │ Email     │ TRC4@USA.COM                                    │
+ │ Author    │ Endrit Pano                                     │
+ │ Email     │ INFO@ALBDROID.AL                                │
+ │ Website   │ https://kodi.al                                 │
+ │ Facebook  │ /albdroid.official/                             │
+ │ Github    │ github.com/SxtBox/                              │
+ │ Created   │ 11 August 2020                                  │
+ │ Modified  │ 23 August 2020                                  │
+ └─────────────────────────────────────────────────────────────┘
+*/
+
+// HOSTED VERSION APIS https://paidcodes.albdroid.al/ZEE5_Dot_Com_Apis/
+
+error_reporting(0);
+set_time_limit(0);
+date_default_timezone_set("Europe/Tirane");
+
+/*
+ Generator @ Kodi dot AL Dev Tools
+ Code For PHP 5/7
+*/
+
+ob_start();
+header("Location: https://paidcodes.albdroid.al/ZEE5_Dot_Com_Apis/Stream_Player_PRO/");
+ob_end_flush();
+
+?>
